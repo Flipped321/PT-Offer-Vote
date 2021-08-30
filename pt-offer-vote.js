2af4ad204a1ad3ba4092a84b3b09aa0122f56714
Flipped <aptx4869zyb@gmail.com>
2021-08-30 14:38:21 +0800

    修改投票方式，大幅提高投票速度

--- pt-offer-vote.js
+++ pt-offer-vote.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PT-Offer-Vote
 // @namespace    http://tampermonkey.net/
-// @version      0.2.0
+// @version      0.3.0
 // @description  主要用于PT站的候选投票
 // @author       Flipped
 // @icon         https://www.google.com/s2/favicons?domain=chdbits.co
@@ -11,26 +11,38 @@
 // @include      https://pt.btschool.club/offers.php
 // @include      https://totheglory.im/viewoffers.php
 // ==/UserScript==
+
 function voteTTG() {
   const nodes = Array.from(document.getElementsByTagName('a')).filter(node => node.innerHTML === '支持')
   nodes.forEach(element => element.click())
+  alert('投票完成。如果稍后弹出大量弹窗提示你已投过票，关闭浏览器tab页即可')
 }
 
 function voteOther() {
   const nodes = document.querySelectorAll("[title='我要！']")
-  const request = new XMLHttpRequest()
+  let requests = []
   Object.keys(nodes).forEach(element => {
-    // 同步执行
-    request.open('GET', nodes[element].href, false)
-    request.send()
+    const request = new XMLHttpRequest()
+    const VoteRequest = new Promise((resolve, reject) => {
+      request.open('GET', nodes[element].href)
+      request.onload = () => resolve()
+      request.send()
+    })
+    requests.push(VoteRequest)
+  })
+
+  Promise.all(requests)
+  .then(res => {
+    alert('全部投票成功。')
+  })
+  .catch(error => {
+    alert('投票完成。但有部分投票失败，可重新尝试')
   })
 }
 
 function startVote() {
   alert('点击确定开始投票')
   isTTG? voteTTG(): voteOther()
-  const EndNotice = `投票完成。${isTTG? '如果稍后弹出大量弹窗提示已投过票，关闭浏览器tab页即可': ''}`
-  alert(EndNotice)
 }
 
 function createVoteOffer() {
